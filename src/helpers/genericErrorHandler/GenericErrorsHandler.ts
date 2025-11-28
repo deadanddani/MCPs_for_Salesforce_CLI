56cada99d5c29436836300bd53f737d71db2c9e2
deadanddnai <deadanddani@gmail.com>
2025-11-28 12:29:07 +0100

    fix error

--- src/helpers/genericErrorHandler/GenericErrorsHandler.ts
+++ src/helpers/genericErrorHandler/GenericErrorsHandler.ts
@@ -29,26 +29,25 @@ function formatError(error: any): CommandError | null {
 
   try {
     const errorMessage = JSON.parse(error.stdout);
+
+    const commandError: CommandError = {
+      name: errorMessage.name,
+      message: errorMessage.message,
+      exitCode: errorMessage.exitCode,
+      actions: errorMessage.actions,
+      context: errorMessage.context,
+      stack: errorMessage.stack,
+      cause: errorMessage.cause,
+      warnings: errorMessage.warnings,
+      code: errorMessage.code,
+      status: errorMessage.status,
+      commandName: errorMessage.commandName,
+    };
+  return commandError;
   } catch {
     // Not a JSON error message
     return null;
   }
-
-  const commandError: CommandError = {
-    name: errorMessage.name,
-    message: errorMessage.message,
-    exitCode: errorMessage.exitCode,
-    actions: errorMessage.actions,
-    context: errorMessage.context,
-    stack: errorMessage.stack,
-    cause: errorMessage.cause,
-    warnings: errorMessage.warnings,
-    code: errorMessage.code,
-    status: errorMessage.status,
-    commandName: errorMessage.commandName,
-  };
-
-  return commandError;
 }
 
 function isCommandError(error: any) {
