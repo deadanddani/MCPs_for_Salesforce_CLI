3462a787454e79f7d0a01d6f72b7d2a9531defd9
deadanddnai <deadanddani@gmail.com>
2026-01-27 12:51:08 +0100

    fix command

--- src/helpers/CommandExecuter.ts
+++ src/helpers/CommandExecuter.ts
@@ -1,8 +1,16 @@
-import { execSync } from "child_process";
+import { spawnSync } from "child_process";
 
 export function executeSync(command: string): string {
-  return execSync(command, { 
+  const result = spawnSync(command, {
     encoding: "utf-8",
-    maxBuffer: 5 * 1024 * 1024 // Increase buffer to 5MB for test executions results
+    maxBuffer: 5 * 1024 * 1024,
+    shell: true,
+    stdio: 'pipe'
   });
+  
+  if(result.status !== 0) {
+    throw new Error(`Command failed with status ${result.status} and message: ${result.stderr}`);
+  }
+
+  return result.stdout ? result.stdout : result.stderr;
 }
